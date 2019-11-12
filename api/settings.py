801ae96125f48b86bb3b4af6e6f1bdc8df4c84c7
Jay Mistry <mistry.jay15@gmail.com>
2019-11-12 13:36:47 +0530

    Add host

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0jvr#p=!9=p@ziy^xkv)wx)33y02zx))-zy7li8ojm!=8_8el&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['201751025.pythonanywhere.com']
 
 
 # Application definition
