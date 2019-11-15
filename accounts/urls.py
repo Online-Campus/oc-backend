bb1b9741ffbabe1ee9158788d2d9f815d677c56e
Jay Mistry <mistry.jay15@gmail.com>
2019-11-15 09:31:58 +0530

    Get current user details

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('register', views.register_view.as_view(), name='register-view'),
-    path('token/obtain', views.TokenObtainView.as_view(), name='token-obtain-view')
+    path('token/obtain', views.TokenObtainView.as_view(), name='token-obtain-view'),
+    path('current_user', views.getUserView.as_view(), name='get-current-user')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,18 +4,28 @@ from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, serializers
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
 from .models import Profile
 from .serializers import ProfileSerializer
 
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class register_view(generics.CreateAPIView):
   permission_classes = (AllowAny,)
 
   queryset = Profile.objects.all()
   serializer_class = ProfileSerializer
+
+class getUserView(APIView):
+  authentication_classes = (JWTAuthentication,)
+  permission_classes = (IsAuthenticated,)
+
+  def get(self, request):
+    user = ProfileSerializer(request.user)
+    return Response({'current_user': user.data})    
 
 class TokenObtainView(TokenObtainPairView):
   permission_classes = (AllowAny,)
